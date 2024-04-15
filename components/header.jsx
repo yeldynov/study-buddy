395d7d297d2854d41548c4786fcc19709e745aaf
Nikita Yeldynov <yeldynov@gmail.com>
2024-04-15 11:42:54 +0300

    * navbar added actual pages and links to them

--- components/header.jsx
+++ components/header.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import Navbar from './navbar'
+import Link from 'next/link'
 
 const Header = () => {
   return (
     <div>
       <div className='h-[488px] bg-[#FEEFD9] bg-opacity-30 flex relative'>
-        <img
-          src='hero-image.png'
-          alt=''
-          className='self-center px-5 lg:px-0 lg:m-[92px] lg:ml-[236px] lg:mr-[273px] lg:mb-[106px] object-contain'
-        />
+        <Link href='/'>
+          <img
+            src='hero-image.png'
+            alt=''
+            className='self-center px-5 lg:px-0 lg:m-[92px] lg:ml-[236px] lg:mr-[273px] lg:mb-[106px] object-contain'
+          />
+        </Link>
       </div>
-      {/* Navbar */}
       <Navbar />
     </div>
   )
--- components/navbar.jsx
+++ components/navbar.jsx
@@ -3,13 +3,12 @@
 import { useState } from 'react'
 import { Dialog, Popover } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
-
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
-}
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 export default function Navbar() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+  const pathname = usePathname()
 
   return (
     <header className='bg-white'>
@@ -30,45 +29,57 @@ export default function Navbar() {
         <Popover.Group className='hidden items-center lg:flex lg:gap-x-12'>
           {/* Navlinks */}
 
-          <a
-            href='#'
-            className='text-lg text-[#818181] hover:bg-[#FEE9CAB3] p-2 px-3 rounded-lg active:bg-[#FBBF67] active:text-black font-ibmplex leading-6 '
+          <Link
+            href='/about'
+            className={`${
+              pathname === '/about' ? 'bg-[#FCCF8D] text-black' : ''
+            } text-lg text-[#818181] font-ibmplex hover:bg-[#FEE9CAB3] p-2 px-3 rounded-lg active:bg-[#FBBF67] active:text-black leading-6 `}
           >
             Про Школу
-          </a>
-          <a
-            href='#'
-            className='text-lg text-[#818181] hover:bg-[#FEE9CAB3] p-2 px-3 rounded-lg active:bg-[#FBBF67] active:text-black leading-6 '
+          </Link>
+          <Link
+            href='/courses'
+            className={`${
+              pathname === '/courses' ? 'bg-[#FCCF8D] text-black' : ''
+            } text-lg text-[#818181] font-ibmplex hover:bg-[#FEE9CAB3] p-2 px-3 rounded-lg active:bg-[#FBBF67] active:text-black leading-6 `}
           >
             Курси
-          </a>
-          <a
-            href='#'
-            className='text-lg text-[#818181] hover:bg-[#FEE9CAB3] p-2 px-3 rounded-lg active:bg-[#FBBF67] active:text-black leading-6 '
+          </Link>
+          <Link
+            href='/testimonials'
+            className={`${
+              pathname === '/testimonials' ? 'bg-[#FCCF8D] text-black' : ''
+            } text-lg text-[#818181] font-ibmplex hover:bg-[#FEE9CAB3] p-2 px-3 rounded-lg active:bg-[#FBBF67] active:text-black leading-6 `}
           >
             Відгуки студентів
-          </a>
-          <a
-            href='#'
-            className='text-lg text-[#818181] hover:bg-[#FEE9CAB3] p-2 px-3 rounded-lg active:bg-[#FBBF67] active:text-black leading-6 '
+          </Link>
+          <Link
+            href='/faq'
+            className={`${
+              pathname === '/faq' ? 'bg-[#FCCF8D] text-black' : ''
+            } text-lg text-[#818181] font-ibmplex hover:bg-[#FEE9CAB3] p-2 px-3 rounded-lg active:bg-[#FBBF67] active:text-black leading-6 `}
           >
             Часті питання
-          </a>
-          <a
-            href='#'
-            className='text-lg text-[#818181] hover:bg-[#FEE9CAB3] p-2 px-3 rounded-lg active:bg-[#FBBF67] active:text-black leading-6 '
+          </Link>
+          <Link
+            href='/testing'
+            className={`${
+              pathname === '/testing' ? 'bg-[#FCCF8D] text-black' : ''
+            } text-lg text-[#818181] font-ibmplex hover:bg-[#FEE9CAB3] p-2 px-3 rounded-lg active:bg-[#FBBF67] active:text-black leading-6 `}
           >
             Тестування
-          </a>
-          <a
-            href='#'
-            className='flex gap-2 h-6 p-5 items-center justify-center text-lg text-[#818181] leading-6 border border-[#FAAF41] rounded-lg shadow-[2px_2px_0px_0px_rgba(255,184,0)] hover:bg-[#FEE9CAB3] active:bg-[#FAAF41] active:text-black  '
+          </Link>
+          <Link
+            href='/subscribe'
+            className={` ${
+              pathname === '/subscribe' ? 'bg-[#FCCF8D] text-black' : ''
+            } flex gap-2 h-6 p-5 items-center justify-center text-lg text-[#818181] font-ibmplex leading-6 border border-[#FAAF41] rounded-lg shadow-[2px_2px_0px_0px_rgba(255,184,0)] hover:bg-[#FEE9CAB3] active:bg-[#FAAF41] active:text-black `}
           >
             <span aria-hidden='true'>
               <img className='h-6 w-6 ' src='/icon_pen.svg' alt='' />
             </span>
             Записатися на урок
-          </a>
+          </Link>
         </Popover.Group>
       </nav>
 
@@ -82,10 +93,10 @@ export default function Navbar() {
         <div className='fixed inset-0 z-10' />
         <Dialog.Panel className='fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10'>
           <div className='flex items-center justify-between'>
-            <a href='#' className='-m-1.5 p-1.5'>
+            <Link href='#' className='-m-1.5 p-1.5'>
               <span className='sr-only'>Study Buddy</span>
               <img className='h-8 w-auto' src='/logo.png' alt='' />
-            </a>
+            </Link>
             <button
               type='button'
               className='-m-2.5 rounded-md p-2.5 text-gray-700'
@@ -98,44 +109,44 @@ export default function Navbar() {
           <div className='mt-6 flow-root'>
             <div className='-my-6 divide-y divide-gray-500/10'>
               <div className='space-y-2 py-6'>
-                <a
-                  href='#'
+                <Link
+                  href='/about'
                   className='-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50'
                 >
                   Про Школу
-                </a>
-                <a
-                  href='#'
+                </Link>
+                <Link
+                  href='/courses'
                   className='-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50'
                 >
                   Курси
-                </a>
-                <a
-                  href='#'
+                </Link>
+                <Link
+                  href='/testimonials'
                   className='-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50'
                 >
                   Відгуки студентів
-                </a>
-                <a
-                  href='#'
+                </Link>
+                <Link
+                  href='/faq'
                   className='-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50'
                 >
                   Часті Питання
-                </a>
-                <a
-                  href='#'
+                </Link>
+                <Link
+                  href='/testing'
                   className='-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50'
                 >
                   Тестування
-                </a>
+                </Link>
               </div>
               <div className='py-6'>
-                <a
-                  href='#'
+                <Link
+                  href='/subscribe'
                   className='-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50'
                 >
                   <span aria-hidden='true'>&rarr;</span> Записатися на урок
-                </a>
+                </Link>
               </div>
             </div>
           </div>
