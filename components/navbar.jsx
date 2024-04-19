5bce2758a254a1bad8d57bf14d647c9208ca5421
Nikita Yeldynov <yeldynov@gmail.com>
2024-04-19 11:40:54 +0300

    fixing homelink in mobile navbar

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -137,7 +137,7 @@ export default function Navbar() {
                 <div className='-my-6 divide-y divide-gray-500/10'>
                   <div className='space-y-2 py-6'>
                     <Link
-                      href='/about'
+                      href='/'
                       className='-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50'
                       onClick={() => setIsOpen(false)}
                     >
